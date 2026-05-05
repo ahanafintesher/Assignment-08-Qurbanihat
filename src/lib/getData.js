6fad032cf600466c9f9dd13defb3af90a5275933
ahanafintesher <ahanafintesher01@gmail.com>
2026-05-05 18:21:46 +0600

    data fetching solved

--- src/lib/getData.js
+++ src/lib/getData.js
@@ -1,5 +1,5 @@
 export async function getData() {
-  const res = await fetch(`http://localhost:3000/data.json`,{ next: { revalidate: 0 } })
+  const res = await fetch(`https://qurbanihat-teal.vercel.app/data.json`,{ cache:"no-store" })
     .then((res) => res.json())
     .catch((err) => {
       console.error("Failed to fetch data:", err);
